a72825c8d1f3eca7a7f44c442268d36a18b09d66
Blooming Yellow <roukhsi69@gmail.com>
2025-02-08 01:31:21 +0100

    modified:   App.tsx 	modified:   android/app/build.gradle 	modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/build.gradle 	modified:   android/gradle.properties 	new file:   assets/images/camera.png 	new file:   assets/images/flame.png 	new file:   assets/images/gas.png 	new file:   assets/images/humi.png     new file:   assets/images/location.png 	new file:   assets/images/logo.png 	new file:   assets/images/temp.png 	modified:   package-lock.json 	modified:   package.json

--- App.tsx
+++ App.tsx
@@ -1,118 +1,277 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Alert,
 } from 'react-native';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const MainScreen = () => {
+  const [flame, setFlame] = useState(70);
+  const [humidity, setHumidity] = useState(50);
+  const [temperature, setTemperature] = useState(30);
+  const [gaz, setGaz] = useState(85);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleCameraPress = () => {
+    Alert.alert('Camera', 'Camera button pressed!');
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const handleLocationPress = () => {
+    Alert.alert('Location', 'Location button pressed!');
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.container}>
+      {/* Top Bar */}
+      <View style={styles.topBar}>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Text style={styles.iconText}>Wi-Fi</Text>
+        </TouchableOpacity>
+        <Image source={require('./assets/images/logo.png')} style={styles.oogo} />
+        <TouchableOpacity style={styles.iconContainer}>
+          <Text style={styles.iconText}>Info</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Map */}
+
+      {/* Sensor Cards */}
+      <View style={styles.cardsContainer}>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Flame</Text>
+          <Text>   </Text>
+          <AnimatedCircularProgress
+            size={100}
+            width={10}
+            fill={flame}
+            tintColor="#90f206"
+            backgroundColor="#3d5875"
+            rotation={-0}
+          >
+            {() => (
+              <Image
+                source={require('./assets/images/flame.png')}
+                style={styles.logo}
+              />
+            )}
+          </AnimatedCircularProgress>
+          <Text>   </Text>
+          <Text style={styles.progressText}>{Math.round(flame)}%</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Humidity</Text>
+          <Text>   </Text>
+          <AnimatedCircularProgress
+            size={100}
+            width={10}
+            fill={humidity}
+            tintColor="#90f206"
+            backgroundColor="#3d5875"
+            rotation={-0}
+          >
+            {() => (
+              <Image
+                source={require('./assets/images/humi.png')}
+                style={styles.logo}
+              />
+            )}
+          </AnimatedCircularProgress>
+          <Text>   </Text>
+          <Text style={styles.progressText}>{Math.round(humidity)}%</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Temperature</Text>
+          <Text>   </Text>
+          <AnimatedCircularProgress
+            size={100}
+            width={10}
+            fill={temperature}
+            tintColor="#90f206"
+            backgroundColor="#3d5875"
+            rotation={-0}
+          >
+            {() => (
+              <Image
+                source={require('./assets/images/temp.png')}
+                style={styles.logo}
+              />
+            )}
+          </AnimatedCircularProgress>
+          <Text>   </Text>
+          <Text style={styles.progressText}>{Math.round(temperature)} °C</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>Gaz</Text>
+          <Text>   </Text>
+          <AnimatedCircularProgress
+            size={100}
+            width={10}
+            fill={gaz}
+            tintColor="#90f206"
+            backgroundColor="#3d5875"
+            rotation={-0}
+          >
+            {() => (
+              <Image
+                source={require('./assets/images/gas.png')}
+                style={styles.logo}
+              />
+            )}
+          </AnimatedCircularProgress>
+          <Text>   </Text>
+          <Text style={styles.progressText}>{Math.round(gaz)}%</Text>
+        </View>
+        <TouchableOpacity style={[styles.card, styles.button2]} onPress={handleCameraPress}>
+          <Text style={styles.cardTitle}>Camera</Text>
+          <Text>   </Text>
+          <AnimatedCircularProgress
+            size={100}
+            width={10}
+            fill={100}
+            tintColor="#90f206"
+            backgroundColor="#3d5875"
+            rotation={-0}
+          >
+            {() => (
+              <Image
+                source={require('./assets/images/camera.png')}
+                style={styles.logo}
+              />
+            )}
+          </AnimatedCircularProgress>
+          <Text>   </Text>
+          <Text>   </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.card, styles.button2]} onPress={handleLocationPress}>
+          <Text style={styles.cardTitle}>Location</Text>
+          <Text>   </Text>
+          <AnimatedCircularProgress
+            size={100}
+            width={10}
+            fill={100}
+            tintColor="#90f206"
+            backgroundColor="#3d5875"
+            rotation={-0}
+          >
+            {() => (
+              <Image
+                source={require('./assets/images/location.png')}
+                style={styles.logo}
+              />
+            )}
+          </AnimatedCircularProgress>
+          <Text>   </Text>
+          <Text>   </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Bottom Buttons */}
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.bottomButton}>
+          <Text style={styles.bottomButtonText}>Settings</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton}>
+          <Text style={styles.bottomButtonText}>Emergency</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  topBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#303030',
+    padding: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
+  oogo: {
+    width: 125,
+    height: 50,
+    marginRight: 0,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginRight: 0,
+  },
+  iconContainer: {
+    padding: 10,
+  },
+  iconText: {
+    color: '#90f206',
+    fontSize: 14,
+  },
+  map: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    color: '#90f206',
+    textAlign: 'center',
+    fontWeight: 'bold',
     fontSize: 18,
-    fontWeight: '400',
   },
-  highlight: {
-    fontWeight: '700',
+  cardsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  card: {
+    width: '30%',
+    backgroundColor: '#1e1e1e',
+    margin: 5,
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  button: {
+    backgroundColor: '#90f206',
+  },
+  button2: {
+    backgroundColor: '#1e1e1e',
+    color: '#90f206',
+  },
+  cardTitle: {
+    color: '#90f206',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  progressText: {
+    color: '#90f206',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  bottomBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#303030',
+    padding: 10,
+  },
+  bottomButton: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+    marginHorizontal: 5,
+    backgroundColor: '#90f206',
+    borderRadius: 10,
+  },
+  bottomButtonText: {
+    color: '#000',
+    fontWeight: 'bold',
   },
 });
 
-export default App;
+export default MainScreen;
